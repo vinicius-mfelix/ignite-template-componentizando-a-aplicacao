3ef97724fb5176dda90e875b944419dc1de0c836
vinicius-mfelix <vinicius.mfelix@outlook.com>
2021-03-19 22:40:02 -0300

    Segregação de componentes SideBar e Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,55 @@
-export function Content() {
+import { useState, useEffect } from 'react';
+import { GenreResponseProps } from '../components/SideBar';
+import { MovieCard } from '../components/MovieCard';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${props.selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [props.selectedGenreId]);
+
+  return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
